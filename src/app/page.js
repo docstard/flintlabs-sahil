2694915a59e1ba59dd69be16fadd5801491cafc4
v3locity47 <aryanmishra530@gmail.com>
2024-01-09 06:59:19 +0530

    First Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,129 @@
-import Image from 'next/image'
+"use client";
+import { InputWithButton } from "@/components/InputWithButton";
+import Image from "next/image";
+import { LineChart, Line, XAxis, YAxis } from "recharts";
+import { useEffect, useState } from "react";
+import ChartContainer from "@/components/ChartContainer";
+import { useToast } from "@/components/ui/use-toast";
+import { Toaster } from "@/components/ui/toaster";
 
+const getAddressMetrics = async (chain, contractAddress, pastHours) => {
+  const reqData = {
+    chain,
+    contractAddress,
+    pastHours,
+  };
+  const res = await fetch("http://localhost:3000/api", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(reqData),
+  });
+  return res.json();
+};
+const PAST_HOURS = 12;
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const { toast } = useToast();
+  const [metrics, setMetrics] = useState({
+    mantle: null,
+    kroma: null,
+    linea: null,
+    newContract: null,
+  });
+  const [addContract, setAddContract] = useState({
+    chain: "",
+    contractAddress: "",
+  });
+  useEffect(() => {
+    const getMetrics = async () => {
+      const mantleMetrics = await getAddressMetrics(
+        "mantle",
+        "0xDCBc586cAb42a1D193CaCD165a81E5fbd9B428d7",
+        PAST_HOURS
+      );
+      const lineaMetrics = await getAddressMetrics(
+        "linea",
+        "0xDCBc586cAb42a1D193CaCD165a81E5fbd9B428d7",
+        PAST_HOURS
+      );
+      const kromaMetrics = await getAddressMetrics(
+        "kroma",
+        "0x7afb9de72A9A321fA535Bb36b7bF0c987b42b859",
+        PAST_HOURS
+      );
+      setMetrics({
+        mantle: mantleMetrics,
+        linea: lineaMetrics,
+        kroma: kromaMetrics,
+      });
+    };
+    getMetrics();
+  }, []);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const alertForChange = {
+    MANTLE: false,
+    LINEA: false,
+    KROMA: false,
+  };
+  useEffect(() => {
+    switch (true) {
+      case metrics?.mantle?.percentageChange <= -10:
+        alertForChange.MANTLE = true;
+      case metrics?.linea?.percentageChange <= -10:
+        alertForChange.LINEA = true;
+      case metrics?.kroma?.percentageChange <= -10:
+        alertForChange.KROMA = true;
+    }
+  }, [metrics.kroma, metrics.linea, metrics.mantle]);
+
+  useEffect(() => {
+    console.log(alertForChange);
+    console.log(metrics);
+    const alertMessages = Object.keys(alertForChange).filter((key) => {
+      return alertForChange[key] === true;
+    });
+    console.log(alertMessages);
+    const message = {
+      title: "Following Contracts balance reduces by 10% in the last 12 hours",
+      description: alertMessages.join(", "),
+    };
+    console.log(message);
+
+    if (alertMessages.length) {
+      toast(message);
+    }
+  }, [alertForChange]);
+
+  const clickHandler = async (e) => {
+    e.preventDefault();
+    const data = await getAddressMetrics(
+      addContract.chain,
+      addContract.contractAddress,
+      PAST_HOURS
+    );
+    setMetrics((prevValues) => ({ ...prevValues, newContract: data }));
+  };
+  return (
+    <main className="flex flex-col justify-between w-full min-h-screen gap-24 px-16 py-24">
+      <ChartContainer title="Mantle" contractMetrics={metrics.mantle} />
+      <ChartContainer title="Linea" contractMetrics={metrics.linea} />
+      <ChartContainer title="Kroma" contractMetrics={metrics.kroma} />
+      <div className="flex flex-col gap-8">
+        <div className="text-5xl ">Add new Address</div>
+        <InputWithButton
+          newMetrics={addContract}
+          setNewMetrics={setAddContract}
+          clickHandler={clickHandler}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {metrics.newContract ? (
+        <ChartContainer
+          title={metrics?.newContract?.chain}
+          contractMetrics={metrics.newContract}
+        />
+      ) : null}
+      <Toaster />
     </main>
-  )
+  );
 }
